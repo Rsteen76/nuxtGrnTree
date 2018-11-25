de5eb6003d26ca2cccc19f50f09f1d035ba21d71
Ryan Steen <ryan@ryansteen.com>
2018-11-25 12:37:53 -0800

    baseURL

--- config/http.js
+++ config/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'sad-spence-9ec68d.netlify.com/api'
+  baseURL: 'https://sad-spence-9ec68d.netlify.com/api'
   // baseURL: 'http://localhost:8080/api'
 })
