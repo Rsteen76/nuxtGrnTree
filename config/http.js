875979c4292f2e006981468fac3bf664c10f0747
Ryan Steen <ryan@ryansteen.com>
2018-11-25 12:25:34 -0800

    Set baseURL

--- config/http.js
+++ config/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  // baseURL: 'https://greentreehive.herokuapp.com/api'
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'sad-spence-9ec68d.netlify.com'
+  // baseURL: 'http://localhost:8080/api'
 })
